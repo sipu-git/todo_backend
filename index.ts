d08a9c52fed41937dd7dfa19830d5e9bc2a01359
Sipu-982 <sipurana38@gmail.com>
2025-11-19 11:19:01 +0530

    origin added

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express()
 app.use(express.json())
 app.use(cors({
-  origin: ["http://localhost:3000", "https://todo-backend-tujg.onrender.com", "https://todo-app-frontend-six-blond.vercel.app", "http://localhost:5000"],
+  origin: ["http://localhost:3000", "https://todo-backend-tujg.onrender.com", "https://todo-frontend-delta-seven.vercel.app", "http://localhost:5000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }))
