1ffc92f46fc44323f75ae18f7555e9d504b99d24
Sipu-982 <sipurana38@gmail.com>
2025-11-19 11:04:03 +0530

    origin added

--- index.ts
+++ index.ts
@@ -10,16 +10,16 @@ dotenv.config();
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:3000","https://todo-backend-1hic.onrender.com","https://todo-app-frontend-six-blond.vercel.app","http://localhost:5000"],
+  origin: ["http://localhost:3000", "https://todo-backend-tujg.onrender.com", "https://todo-app-frontend-six-blond.vercel.app", "http://localhost:5000"],
   credentials: true,
-  methods:["GET","POST","PUT","DELETE"]
+  methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 app.use(cookieParser());
-app.use('/api/user',UserRoutes);
-app.use('/api/task',TaskRoutes);
+app.use('/api/user', UserRoutes);
+app.use('/api/task', TaskRoutes);
 DatabaseConnect()
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`The server is activating on ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`The server is activating on ${PORT}`)
 })          
